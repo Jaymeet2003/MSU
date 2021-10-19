7365218d56c0e87b987be8ba44beca608ebbd8f6
Jaymeet Patel <54699147+Jaymeet2003@users.noreply.github.com>
2021-10-19 13:06:05 -0500

    Delete ceo directory

--- ceo/deletehistory.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include '../db.php';
-    $s2_query = "DELETE FROM historytab WHERE h_id = '".$_GET['id']."';";
-    $q2_run = mysqli_query($conn, $s2_query);
-    header('location:historytask.php');
-?>
